938c44ce35b8d7856ed7de99fc539b1c6a8d81fd
X <mo_noki@yahoo.com>
2019-11-27 16:55:22 +0900

    Update Reverse Integer.js

--- leetcode/Reverse Integer.js
+++ leetcode/Reverse Integer.js
@@ -6,10 +6,10 @@ var reverse = function (x) {
     if (x < Math.pow(-2, 31) || x > (Math.pow(2, 31) - 1)) {
         return 0;
     }
-
     var arr = x.toString(10).replace(/\D/g, '0').split('').map(Number);
-    if (arr[0] === -1) {
-        return Number(arr.shift().reverse().join(""));
+    if (arr[0] ===0) {
+        arr.shift();
+        return -Number(arr.reverse().join(""));
     }
     else {
         return Number(arr.reverse().join(""));
